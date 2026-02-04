a8a4b76dfd234f7dac146202aa95e30abebaf5f9
Dardeniuk <kondratiukdanys@gmail.com>
2026-02-04 20:28:20 +0200

    third commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -108,17 +108,14 @@ public class App {
         if (logger.isLoggable(Level.INFO)) {
             logger.log(Level.INFO, """
                             
-                            {0} | {1} | {2}
+                            %s | %s | %s
                             -----------
-                            {3} | {4} | {5}
+                            %s | %s | %s
                             -----------
-                            {6} | {7} | {8}
-                            """,
-                    new Object[]{
-                            box[0], box[1], box[2],
-                            box[3], box[4], box[5],
-                            box[6], box[7], box[8]
-                    });
+                            %s | %s | %s
+                            """.formatted(box[0], box[1], box[2],
+                    box[3], box[4], box[5],
+                    box[6], box[7], box[8]));
         }
     }
 
